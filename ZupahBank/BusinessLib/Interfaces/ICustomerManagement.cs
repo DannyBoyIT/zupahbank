b1225715aa181a0b17b44bb691456d2afd411893
Tanja Svärd <tanja_jazz@hotmail.com>
2017-11-01 11:54:30 +0100

    Added interface for customer

---
+++ ZupahBank/BusinessLib/Interfaces/ICustomerManagement.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLib.Interfaces
+{
+    interface ICustomerManagement
+    {
+        void Create();
+        void Delete();
+        Customer Search(string searchText);
+        Customer ShowCustomer(int id);
+    }
+}
