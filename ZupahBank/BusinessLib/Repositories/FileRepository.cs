b6f9bf38d0f86036ae791770806e6328c9ffc015
isabellaalstrom <isabella.alstrom@gmail.com>
2017-11-01 13:25:47 +0100

    Added FileService. Removed IRepository and IFileRepository

--- ZupahBank/BusinessLib/Repositories/FileRepository.cs
+++ ZupahBank/BusinessLib/Repositories/FileRepository.cs
@@ -3,27 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BusinessLib.Interfaces;
+
 using BusinessLib.Models;
 
 namespace BusinessLib.Repositories
 {
-    public class FileRepository : IRepository
+    public class FileService
     {
-        public void CreateCustomer(string customerName, string legalId, string address, string zipCode, string city, string region,
-            string country, string phoneNumber)
-        {
-            throw new NotImplementedException();
-        }
 
-        public void CreateAccount(decimal balance, int customerId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CreateTransaction(decimal amount, Account toAccount, Account fromAccount)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
