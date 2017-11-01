b653bede3a508792b9d1cc7e216d34affe75e410
DannyBoy <d_iliovits@windowslive.com>
2017-11-01 16:18:38 +0100

    AcountManagement interface created.

---
+++ ZupahBank/BusinessLib/Interfaces/IAccountManagement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLib.Interfaces
+{
+    public interface IAccountManagement
+    {
+        void Create(int customerId);
+        void Delete(int accountId);
+    }
+}
