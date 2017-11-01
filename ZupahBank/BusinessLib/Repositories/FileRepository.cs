178bf1e76c492f81efce8355f1b962574506426f
isabellaalstrom <isabella.alstrom@gmail.com>
2017-11-01 13:17:38 +0100

    Added IRepository with create-methods for account, customer, transaction. Added FileRepository that implements IRepository.

---
+++ ZupahBank/BusinessLib/Repositories/FileRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLib.Interfaces;
+using BusinessLib.Models;
+
+namespace BusinessLib.Repositories
+{
+    public class FileRepository : IRepository
+    {
+        public void CreateCustomer(string customerName, string legalId, string address, string zipCode, string city, string region,
+            string country, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateAccount(decimal balance, int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateTransaction(decimal amount, Account toAccount, Account fromAccount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
