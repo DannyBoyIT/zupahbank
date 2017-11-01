9533efe2a19491c6f7fd61e55bb8d255ef726f2f
Klas <klas793@gmail.com>
2017-11-01 11:02:32 +0100

    Initial transaction model

---
+++ ZupahBank/BusinessLib/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLib.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int Amount { get; set; }
+        // TODO: Add to- and from-account
+    }
+}
