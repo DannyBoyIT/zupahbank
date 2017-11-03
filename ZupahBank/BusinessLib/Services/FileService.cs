14915458a19b1f97fbe6ec37b98ef3dab655e884
isabellaalstrom <isabella.alstrom@gmail.com>
2017-11-03 13:29:58 +0100

    Added files. Added FileService-class

---
+++ ZupahBank/BusinessLib/Services/FileService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BusinessLib.Models;
+
+namespace BusinessLib.Services
+{
+    public class FileService
+    {
+        public string ReadFromFile()
+        {
+            try
+            {
+                using (var sr = new StreamReader("bankdata-small.txt"))
+                {
+                    foreach (var line in sr.ReadLine())
+                    {
+                        
+                    }
+                    // Read the stream to a string, and write the string to the console.
+                    var file = sr.ReadToEnd();
+                    return file;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public void WriteNewFile()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
