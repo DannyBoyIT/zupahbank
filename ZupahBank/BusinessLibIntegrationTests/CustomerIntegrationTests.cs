d350d2cab517457142569d0f5533f222835d6485
Klas <klas793@gmail.com>
2017-11-08 16:11:03 +0100

    Customer integration tests moved to new file

---
+++ ZupahBank/BusinessLibIntegrationTests/CustomerIntegrationTests.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLib.Repositories;
+using BusinessLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace BusinessLibIntegrationTests
+{
+    class CustomerIntegrationTests
+    {
+        [Fact]
+        public void CanSearchForCustomer()
+        {
+            var sut = FileRepository.Instance;
+
+            sut.CreateCustomer("Kalle Kallesson", "801010-1010", "Långgatan 1", "11122", "Huvudsta", "Stockholm", "Sverige", "010111222");
+
+            var customers = sut.SearchCustomer("Kalle");
+
+            Assert.Equal(1, customers.Count);
+        }
+
+        [Fact]
+        public void NewCustomerGetsAValidId()
+        {
+            var sut = FileRepository.Instance;
+            var service = new FileService();
+            const string path = @".\Files\bankdata-small.txt";
+            service.TransformFileToLists(sut, path);
+
+            sut.CreateCustomer("Kalle Kallesson", "801010-1010", "Långgatan 1", "11122", "Huvudsta", "Stockholm", "Sverige", "010111222");
+
+            Assert.Equal(1033, sut.Customers.LastOrDefault()?.CustomerId);
+        }
+    }
+}
