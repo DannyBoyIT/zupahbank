cdf40b036866e01bdd37061bf3e62ddd247d8e26
DannyBoy <d_iliovits@windowslive.com>
2017-11-09 21:44:57 +0100

    Moved CustomerManagement to the System folder

--- ZupahBank/BusinessLib/System/CustomerManagement.cs
+++ ZupahBank/BusinessLib/System/CustomerManagement.cs
@@ -1,12 +1,9 @@
 ﻿using BusinessLib.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BusinessLib.Models;
 
-namespace BusinessLib.Repositories
+namespace BusinessLib.System
 {
     class CustomerManagement : ICustomerManagement
     {
