2a84203f0094ceb8e30a82b3e3fbf9c93cf103d1
Tanja Svärd <tanja_jazz@hotmail.com>
2017-11-13 22:11:34 +0100

    No Changes (BankSystem.cs)

---
+++ ZupahBank/BusinessLibIntegrationTests/BankSystem.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLib.Interfaces;
+
+namespace BusinessLib.System
+{
+    public class BankSystem
+    {
+        public readonly AccountManagement accountManagement;
+        public readonly CustomerManagement customerManagement;
+        public readonly TransactionManagement transactionManagement;
+
+        public BankSystem(IRepository repository)
+        {
+            accountManagement = new AccountManagement(repository);
+            customerManagement = new CustomerManagement(repository);
+            transactionManagement = new TransactionManagement(repository);
+        }
+    }
+}
+
